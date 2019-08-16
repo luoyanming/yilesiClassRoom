d922a14eccff391223dea3ecce20bafc3caa58e6
luoym <13575458746@163.com>
2019-08-16 11:08:13 +0800

    答题宝

--- dev/static/script/util.js
+++ dev/static/script/util.js
@@ -15,6 +15,9 @@ window.CONFIG = {
     // online: '118.31.22.134:8095/ws',
     // apiOnline: 'http://ctest.izhihuidao.com'
 
-    online: '118.31.22.134:8091/ws',
-    apiOnline: 'http://c.izhihuidao.com'
+    // online: '118.31.22.134:8091/ws',
+    // apiOnline: 'http://c.izhihuidao.com'
+
+    online: 'datibao.yilesi.cn:8091/ws',
+    apiOnline: 'http://datibao.yilesi.cn/'
 };
---
+++ dist/static/script/util-34dbbf70c5.js
@@ -0,0 +1,7 @@
+/**
+ * @author luoym
+ * @email 13575458746@163.com
+ * @descrip 
+ * @version v2.5.0
+ */
+"use strict";window.CONFIG={version:"2.5.0",online:"datibao.yilesi.cn:8091/ws",apiOnline:"http://datibao.yilesi.cn/"};
